ffe14821847bf1ddfce5ebfd39f48a1919bd87b1
Nhatgoodkid <dcn22402@gmail.com>
2024-08-28 19:31:37 +0700

    Update vercel.json

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9_$%84t1e*f2ibhn_z9fp9z$d495775^t6^kjns4u$9jt^j#q1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 
 # Application definition
@@ -41,9 +41,10 @@ INSTALLED_APPS = [
     'human',
 ]
 CORS_ORIGIN_ALLOW_ALL= True
+
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
