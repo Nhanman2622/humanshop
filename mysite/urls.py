dee0e9e1d8f8ea267ecf1c0dbe1d574938c833b3
Nhatgoodkid <dcn22402@gmail.com>
2024-08-28 19:58:32 +0700

    update code

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('human.urls')),
 ]
 handler404 = 'human.views.custom_404'
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -18,18 +18,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 # Kết nối tới MongoDB
+connect(host='mongodb+srv://nguyennhan2682007:uhxmCzqNWZcpWUgZ@cluster0.cjwkw2x.mongodb.net/')
 
-def check_mongodb_connection():
-    try:
-        # Attempt to connect to MongoDB
-        
-        connect(host='mongodb+srv://nguyennhan2682007:uhxmCzqNWZcpWUgZ@cluster0.cjwkw2x.mongodb.net/')
-        print("Successfully connected to MongoDB!")
-    except Exception as e:
-        print(f"Failed to connect to MongoDB. Error: {e}")
-
-# Call the function to check the connection
-check_mongodb_connection()
 application = get_wsgi_application()
 
 app = application
